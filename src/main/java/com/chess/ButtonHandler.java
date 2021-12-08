cae46016dca5b342d5f7381bd1d1f7af063ae321
Philip-Jan <philipjans255@gmail.com>
2021-12-08 13:02:51 +0100

    Commit

--- src/main/java/com/chess/ButtonHandler.java
+++ src/main/java/com/chess/ButtonHandler.java
@@ -4,8 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 
-import java.util.Objects;
-
 public class ButtonHandler implements EventHandler<ActionEvent> {
     private final int number;
     private final Button button;
@@ -18,44 +16,32 @@ public class ButtonHandler implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent event) {
-        if (Objects.equals(ChessPiece, "")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Witte Koning")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Witte Koningin")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Witte Loper")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Wit Paard")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Witte Toren")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Witte Pion")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Zwarte Koning")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Zwarte Koningin")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Zwarte Loper")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Zwart Paard")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Zwarte Toren")) {
-
-        }
-        else if (Objects.equals(ChessPiece, "Zwarte Pion")) {
-
+        switch (ChessPiece) {
+            case "WK":
+                break;
+            case "WD":
+                break;
+            case "WT":
+                break;
+            case "WP": //Paard
+                break;
+            case "WL":
+                break;
+            case "Wp": //pion
+                break;
+            case "ZK":
+                break;
+            case "ZD":
+                break;
+            case "ZT":
+                break;
+            case "ZP":
+                break;
+            case "ZL":
+                break;
+            case "Zp":
+                break;
+            default:
         }
     }
 
